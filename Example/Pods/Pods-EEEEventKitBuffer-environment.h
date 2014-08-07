61621b87480f46ad85e4c903526f27718d2a90d4
Eric-Paul Lecluse <e@epologee.com>
2014-08-07 19:05:34 +0200

    Add Core Data model as a bundled resource to the podspec

---
+++ Example/Pods/Pods-EEEEventKitBuffer-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// EEEEventKitBuffer
+#define COCOAPODS_POD_AVAILABLE_EEEEventKitBuffer
+#define COCOAPODS_VERSION_MAJOR_EEEEventKitBuffer 0
+#define COCOAPODS_VERSION_MINOR_EEEEventKitBuffer 1
+#define COCOAPODS_VERSION_PATCH_EEEEventKitBuffer 0
+
